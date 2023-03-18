74b89b6b04b69dbb977510422ad0d608474a8581
Mariana <nanomarii10@gmail.com>
2023-03-18 10:22:49 -0300

    feat: initial setup and header implementation

---
+++ src/pages/History.tsx
@@ -0,0 +1,8 @@
+export function History() {
+  return (
+    <div>
+      <div>History</div>
+      <p>teste</p>
+    </div>
+  )
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export function Home() {
+  return (
+    <div>
+      <div>Home</div>
+      <p>teste</p>
+    </div>
+  )
+}
