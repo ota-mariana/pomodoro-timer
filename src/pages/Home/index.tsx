bc283125d6a97fca8bd95535bf79a4e4078b7883
Mariana <nanomarii10@gmail.com>
2023-03-20 08:56:25 -0300

    add forms validation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,7 @@
 import { Play } from 'phosphor-react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+import { useForm } from 'react-hook-form'
 import {
   CountDownContainer,
   FormContainer,
@@ -9,36 +12,62 @@ import {
   MinutesAmountInput,
 } from './styles'
 
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod.number().min(5, 'O ciclo precisa ser de mínimo 5 minutos'),
+})
+
+type NewFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 export function Home() {
+  const { register, handleSubmit, watch } = useForm<NewFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: { task: '', minutesAmount: 0 },
+  })
+
+  const handleChange = (data: NewFormData) => {
+    console.log(data)
+  }
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
+
   return (
     <HomeContainer>
-      <FormContainer>
-        <label htmlFor="task">Vou trabalhar em</label>
-        <TaskInput id="task" placeholder="Dê um nome para seu projeto" />
+      <form onSubmit={handleSubmit(handleChange)} action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            id="task"
+            placeholder="Dê um nome para seu projeto"
+            {...register('task')}
+          />
 
-        <label htmlFor="">Durante</label>
-        <MinutesAmountInput
-          type="number"
-          id="minutesAmount"
-          placeholder="00"
-          step={5}
-        />
+          <label htmlFor="">Durante</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            {...register('minutesAmount', { valueAsNumber: true })}
+          />
 
-        <span>minutos</span>
+          <span>minutos</span>
 
-        <CountDownContainer>
-          <span>0</span>
-          <span>0</span>
-          <Separator>:</Separator>
-          <span>0</span>
-          <span>0</span>
-        </CountDownContainer>
+          <CountDownContainer>
+            <span>0</span>
+            <span>0</span>
+            <Separator>:</Separator>
+            <span>0</span>
+            <span>0</span>
+          </CountDownContainer>
 
-        <StartCountDownButton disabled type="submit">
-          <Play size={24} />
-          Começar
-        </StartCountDownButton>
-      </FormContainer>
+          <StartCountDownButton disabled={isSubmitDisabled} type="submit">
+            <Play size={24} />
+            Começar
+          </StartCountDownButton>
+        </FormContainer>
+      </form>
     </HomeContainer>
   )
 }
